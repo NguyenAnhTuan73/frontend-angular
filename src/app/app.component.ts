d0e576bfd7254912a401f977b41e89c3fb55e288
tuan_dev_qb <nguyentuanqb73@gmail.com>
2023-10-12 16:59:58 +0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Product } from './common/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend-angular';
+  product: Product = {
+    name: 'Hat',
+    quantity: 200,
+    price: 500,
+  };
 }
